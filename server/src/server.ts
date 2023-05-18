3fa6aacad4191c958376554595d5895d68cf3222
jones-bass <jonesbass.tb@gmail.com>
2023-05-17 21:31:26 -0300

    Crud back

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,19 @@
 import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
+import { fastifyCors } from '@fastify/cors'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'hello world'
+app.register(fastifyCors, {
+  origin: true,
 })
+
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP Server Running on 3333')
