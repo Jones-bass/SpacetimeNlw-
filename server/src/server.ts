99d53c528932787bb7338b739afa8341afd70d36
jones-bass <jonesbass.tb@gmail.com>
2023-05-19 19:30:57 -0300

    Rota upload Back

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,8 +5,18 @@ import { memoriesRoutes } from './routes/memories'
 import { fastifyCors } from '@fastify/cors'
 import { authRoutes } from './routes/auth'
 import jwt from '@fastify/jwt'
+import { resolve } from 'node:path'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(fastifyCors, {
   origin: true,
@@ -17,6 +27,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
